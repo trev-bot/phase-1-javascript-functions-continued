af52ce0daed34c593b595d26e38dbb30895b168a
Trevor <duncantrev@gmail.com>
2022-01-25 17:58:24 -0500

    commit tree 5da4887dede73d362312bdcced84672f5653408e

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+   return(`This Saturday, I want to ${activity}!`);
+}
+
+const mondayWork = function (activity = "go to the office"){
+    return(`This Monday, I will ${activity}.`)
+
+}
+
+// declaration - defined using the keywork function- could have a nname or no name (annonomous)
+// function wrapAdjective (){
+
+// }
+
+// expression - a variable that is assigned the value of an annonomous function (function with no name)
+function wrapAdjective(visualFlair = "*"){
+    return function (param1 = "special",){
+        return (`You are ${visualFlair}${param1}${visualFlair}!`)
+
+    }
+
+ }
+ console.log(wrapAdjective()())
+
+
+
+
+
